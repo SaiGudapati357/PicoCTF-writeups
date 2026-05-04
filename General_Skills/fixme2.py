c1ec217768add6231536a88518ad56c56ab68ec8
SaiGudapati357 <147217332+SaiGudapati357@users.noreply.github.com>
2026-05-04 02:45:56 -0400

    Create fixme2.py

---
+++ General_Skills/fixme2.py
@@ -0,0 +1,8 @@
+## fixme2.py writeup
+
+
+## Steps
+## 1. Go to the webshell and type in this command: wget https://artifacts.picoctf.net/c/5/fixme2.py
+## 2. run python3 fixme2.py to see if it executes
+## 3. The issue is that there is incorrect syntax at "flag = "" " it should be "==" instead of "="
+## 4. run python3 fixme2.py again and it should execute properly and give the flag
